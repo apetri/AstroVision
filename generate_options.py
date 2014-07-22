0752e794b9aca83488a7913942bc9a1407dc2184
apetri <apetri@phys.columbia.edu>
2014-07-22 13:12:58 -0600

    works in python2

--- generate_options.py
+++ generate_options.py
@@ -1,16 +1,17 @@
-#Runs only in python3!
-import sys,configparser
+import sys,ConfigParser
 
-options = configparser.ConfigParser()
-options["simulations"] = {"root_path":"/Users/andreapetri/Documents/Columbia/spurious_shear/convergence_maps"}
-options["analysis"] = {
+options = ConfigParser.ConfigParser()
+
+options.add_section("simulations") 
+options.set("simulations","root_path","/Users/andreapetri/Documents/Columbia/spurious_shear/convergence_maps")
+
+options.add_section("analysis") 
 	
-	"num_realizations" : "3",
-	"smoothing_scales" : "0.1,0.5,1.0",
-	"bin_edges" : "np.ogrid[-0.15:0.15:15j]",
-	"redshift" : "1.0"
+options.set("analysis","num_realizations","3")
+options.set("analysis","smoothing_scales","0.1,0.5,1.0")
+options.set("analysis","bin_edges","np.ogrid[-0.15:0.15:15j]")
+options.set("analysis","redshift","1.0")
 
-}
 
 with open(sys.argv[1],"w") as configfile:
 	options.write(configfile)
